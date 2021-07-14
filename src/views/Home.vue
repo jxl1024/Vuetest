6fda8a4587eb134af8cd84eb0776bfa7e27098ef
jiangxl <jiangxl@hicore.com>
2021-07-14 09:40:53 +0800

    添加Dockerfile文件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="test project" />
+    <HelloWorld msg="使用Jenkins自动化部署Vue到docker" />
   </div>
 </template>
 
